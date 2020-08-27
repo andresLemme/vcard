e3dfad3a3bf94ea9b8318ebfdd86799ed9498bf3
Pocas1982 <andres.lemme@gmail.com>
2020-08-27 17:07:04 -0300

    add background color and avatar box

--- src/Components/CardAvatar/CardAvatar.js
+++ src/Components/CardAvatar/CardAvatar.js
@@ -4,6 +4,14 @@ import './CardAvatar.scss'
 function CardAvatar() {
   return(
     <div className="avatar-card">
+    <div className="avatar">
+      <img src="https://i.postimg.cc/445zy52N/perfil-Avatar.jpg"/>
+    </div>
+    <div className="info-box">
+      <h1 className="title">Dr. Sebastián Santamarina</h1>
+      <p className="description">Médico Clínico</p>
+      <p className="description">MP: 1234</p>
+    </div>
    
     </div>
   )
