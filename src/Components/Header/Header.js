d0ec5e4c523821e94fd88b77952e7853af71c0c6
Pocas1982 <andres.lemme@gmail.com>
2020-08-30 20:39:05 -0300

    image added to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.scss'
 import CardAvatar from '../CardAvatar/CardAvatar'
+import Line from '../Commons/img/sp_main_bg.png'
 
 
 
 function Header(){
   return(
-    <div className="bg-box">
+    <div className="bg-box" >
     <CardAvatar/>
 
     </div>
